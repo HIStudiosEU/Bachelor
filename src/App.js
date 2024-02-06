744b241e1bd714afab8ce091c1385a96b702076b
HIStudiosEU <s0574700@htw-berlin.de>
2024-02-06 12:24:31 +0100

    erster commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-
+import TaskList from './components/TaskList';
+import Calculator from './components/Calculator';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1 style={{textAlign: 'center', marginTop: '25px', marginBottom: '25px'}}>MyUsefulApp</h1>
+    <Calculator />
+    <TaskList />
+    </>
   );
 }
 
---
+++ src/components/__tests__/Calculator.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Calculator from '../Calculator';
+
+test('Calculator handles addition correctly', () => {
+  // Render Calculator-Komponente
+  const { getByText, getByTestId } = render(<Calculator />);
+
+  // Klicke auf die Tasten 2, +, 3 und =
+  fireEvent.click(getByText('2'));
+  fireEvent.click(getByText('+'));
+  fireEvent.click(getByText('3'));
+  fireEvent.click(getByText('='));
+
+  // Überprüfe, ob die Eingabe und das Ergebnis korrekt sind
+  expect(getByTestId('input-display')).toHaveTextContent('2+3');
+  expect(getByTestId('result-display')).toHaveTextContent('5');
+});
+
+// Füge weitere Tests hinzu, um andere Rechenoperationen und Randfälle zu überprüfen.
